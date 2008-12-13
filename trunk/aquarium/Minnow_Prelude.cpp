37ad4efa063a3772f2d401a545bc4a66ba889b3b
jonathan.d.turner <jonathan.d.turner@192a147a-2652-0410-b9c2-9f4538f363dd>
2008-12-13 00:18:44 +0000

    Adding in some missing standard lib functions.

--- trunk/aquarium/Minnow_Prelude.cpp
+++ trunk/aquarium/Minnow_Prelude.cpp
@@ -46,3 +46,109 @@ int convert_s_to_i__(Typeless_Vector__ *s) {
 
     return retval;
 }
+
+float convert_s_to_f__(Typeless_Vector__ *s) {
+    if (s == NULL) return 0;
+    float retval;
+    push_onto_char_string__(s, 0);
+    retval = atof((char *)(s->contents));
+    pop_off_char_string__(s);
+
+    return retval;
+}
+
+double convert_s_to_d__(Typeless_Vector__ *s) {
+    if (s == NULL) return 0;
+    double retval;
+    push_onto_char_string__(s, 0);
+    retval = atof((char *)(s->contents));
+    pop_off_char_string__(s);
+
+    return retval;
+}
+
+char convert_s_to_c__(Typeless_Vector__ *s) {
+    if (s == NULL) return 0;
+
+    if (s->current_size == 0)
+        return 0;
+    else
+        return ((char *)s->contents)[0];
+}
+
+Typeless_Vector__ *convert_i_to_s__(int i) {
+    Typeless_Vector__ *tv = create_char_string__(10);
+    sprintf((char*)tv->contents, "%i", i);
+    pop_off_typeless_vector__(tv); //remove the trailing 0
+    return tv;
+}
+
+float convert_i_to_f__(int i) {
+    return (float)i;
+}
+
+double convert_i_to_d__(int i) {
+    return (double)i;
+}
+
+char convert_i_to_c__(int i) {
+    return (char)i;
+}
+
+Typeless_Vector__ *convert_f_to_s__(float f) {
+    Typeless_Vector__ *tv = create_char_string__(10);
+    sprintf((char*)tv->contents, "%f", f);
+    pop_off_typeless_vector__(tv); //remove the trailing 0
+    return tv;
+}
+
+int convert_f_to_i__(float f) {
+    return (int)f;
+}
+
+double convert_f_to_d__(float f) {
+    return (double)f;
+}
+
+char convert_f_to_c__(float f) {
+    return (char)f;
+}
+
+Typeless_Vector__ *convert_d_to_s__(double d) {
+    Typeless_Vector__ *tv = create_char_string__(10);
+    sprintf((char*)tv->contents, "%f", d);
+    pop_off_typeless_vector__(tv); //remove the trailing 0
+    return tv;
+}
+
+int convert_d_to_i__(double d) {
+    return (int)d;
+}
+
+float convert_d_to_f__(double d) {
+    return (float)d;
+}
+
+char convert_f_to_c__(double d) {
+    return (char)d;
+}
+
+Typeless_Vector__ *convert_c_to_s__(char c) {
+    Typeless_Vector__ *tv = create_char_string__(10);
+    sprintf((char*)tv->contents, "%c", c);
+    pop_off_typeless_vector__(tv); //remove the trailing 0
+    return tv;
+}
+
+int convert_c_to_i__(char c) {
+    return (int)c;
+}
+
+double convert_c_to_d__(char c) {
+    return (double)c;
+}
+
+float convert_c_to_f__(char c) {
+    return (float)c;
+}
+
