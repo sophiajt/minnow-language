7766f3b181ba3f23093bd129aefae06c3a52540a
jonathan.d.turner <jonathan.d.turner@192a147a-2652-0410-b9c2-9f4538f363dd>
2008-12-27 21:09:08 +0000

    Added the ability to unit test for error messages.

--- trunk/run_unittests.py
+++ trunk/run_unittests.py
@@ -3,7 +3,11 @@ import sys
 import os
 from subprocess import *
 
-unittest_dir = "unittests"
+if (len(sys.argv) > 1):
+  unittest_dir = sys.argv[1]
+else:
+  unittest_dir = "unittests"
+
 binary_dir = "tmp"
 out_bin = os.path.join(binary_dir, "unitout")
 compiler_dir = "bin"
@@ -32,25 +36,47 @@ def test_directory(d):
       else:
         test_directory(os.path.join(d, f))
         print("")
+    elif f[-3:] == "err":
+      pass 
     elif f[-3:] == "tst":
-      pass #print("Test found: " + f)
+      pass 
     else:
       total_test_count += 1
       s = os.path.join(d, f)
       tst = s[:-3] + "tst"
+      err = s[:-3] + "err"
+
+      tst_cmp = ""
+      err_cmp = ""
+
+      if (os.path.isfile(tst)):
+        tst_file = open(tst, 'r')
+        tst_cmp = tst_file.read()
+
+      if (os.path.isfile(err)):
+        err_file = open(err, 'r')
+        err_cmp = err_file.read()
+
       output = Popen([compiler_bin, s,  "-o", out_bin], stdout=PIPE, stderr=PIPE).communicate()
+
       if (len(output[1]) > 0):
-        failed_tests.append( (s, output[1]) )
-        tick_bad()
+        if (err_cmp != output[1]):
+          failed_tests.append( (s, output[1]) )
+          tick_bad()
+        else:
+          tick_good()
       else:
         if (os.path.isfile(tst)):
           tst_file = open(tst, 'r')
-
           tst_cmp = tst_file.read()
+
           bin_output = Popen([out_bin], stdout=PIPE, stderr=PIPE).communicate()
           if (len(bin_output[1]) > 0):
-            failed_tests.append( (s, bin_output[1]) )
-            tick_bad()
+            if (err_cmp != output[1]):
+              failed_tests.append( (s, bin_output[1]) )
+              tick_bad()
+            else:
+              tick_good()
           elif (tst_cmp != bin_output[0]):
               failed_tests.append( (s, bin_output[0]) )
               tick_bad()
@@ -61,6 +87,7 @@ def test_directory(d):
           tick_bad()
 
 test_directory(unittest_dir)
+print("")
 if (len(failed_tests) > 0):
   print("Failed tests: " + str(len(failed_tests)) + " of " + str(total_test_count))
   for i in failed_tests:
